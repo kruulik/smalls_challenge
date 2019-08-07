d366fad8d86e5ce2a1ae98ee51c27241c622f5f0
kruulik <karoliskrulis@gmail.com>
2019-08-07 19:24:32 -0400

    parse json for :resource/:id

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,9 +1,29 @@
 class ResourcesController < ApplicationController 
 
+    def parse_resource(resource)
+        parsedResource = {
+            data: {
+                attributes: {},
+                relationships: {}
+            },
+        }
+        resource.each do |key, value| 
+            if (value.instance_of? Array) || (value.include?("swapi.co/api"))
+                parsedResource[:data][:relationships][key] = {
+                    links: value
+                }
+            else
+                parsedResource[:data][:attributes][key] = value
+            end
+        end
+        parsedResource
+    end
+
     def get_resource
         response_object = Request.new('https://swapi.co/api')
         @resource  = response_object.get_resource(params[:resource], params[:id])
-        render json: @resource
+        parsed = parse_resource(@resource)
+        render json: parsed
     end
     
     def list_resources
