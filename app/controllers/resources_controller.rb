a57fed6ad3d750035e637f0ef83de0fa2210abf7
kruulik <karoliskrulis@gmail.com>
2019-08-07 19:48:22 -0400

    parse results array

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -2,33 +2,40 @@ class ResourcesController < ApplicationController
 
     def parse_resource(resource)
         parsedResource = {
-            data: {
-                attributes: {},
-                relationships: {}
-            },
+            attributes: {},
+            relationships: {}
         }
         resource.each do |key, value| 
             if (value.instance_of? Array) || (value.include?("swapi.co/api"))
-                parsedResource[:data][:relationships][key] = {
+                parsedResource[:relationships][key] = {
                     links: value
                 }
             else
-                parsedResource[:data][:attributes][key] = value
+                parsedResource[:attributes][key] = value
             end
         end
-        parsedResource
+        return parsedResource
+    end
+    
+    def parse_list(list) 
+        parsed = []
+        list.each do |item|
+            parsed << parse_resource(item)
+        end 
+        return {data: parsed}
     end
 
     def get_resource
         response_object = Request.new('https://swapi.co/api')
         @resource  = response_object.get_resource(params[:resource], params[:id])
         parsed = parse_resource(@resource)
-        render json: parsed
+        render json: {data: parsed}
     end
     
     def list_resources
         response_object = Request.new('https://swapi.co/api')
-        @resource  = response_object.list_resources(params[:resource])
-        render json: @resource
+        @resource = response_object.list_resources(params[:resource])["results"]
+        parsed = parse_list(@resource)
+        render json: parsed
     end
 end
